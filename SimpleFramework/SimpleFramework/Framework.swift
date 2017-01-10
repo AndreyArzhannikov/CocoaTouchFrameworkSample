19e927aefebd24c8bdac64bc34dbe2819585ef4c
Andrey Arzhannikov <andreya@handsome.is>
2017-01-10 18:23:15 +0600

    Create framework

---
+++ SimpleFramework/SimpleFramework/Framework.swift
@@ -0,0 +1,13 @@
+//
+//  Framework.swift
+//  SimpleFramework
+//
+//  Created by Andrey Arzhannikov on 09.01.17.
+//  Copyright © 2017 Andrey Arzhannikov. All rights reserved.
+//
+
+import Foundation
+
+func provideGreeting() -> String {
+    return "Hello from Simple Framework!"
+}
